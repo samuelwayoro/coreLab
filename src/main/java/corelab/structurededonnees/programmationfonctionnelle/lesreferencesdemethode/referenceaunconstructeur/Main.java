3e53cdff9a88d4b321404811f32958aa0e624898
Samuel WAYORO <48677219+samuelwayoro@users.noreply.github.com>
2025-06-05 11:52:44 +0200

    add : methode de reference pour l'utilisation de construction

--- src/main/java/corelab/structurededonnees/programmationfonctionnelle/lesreferencesdemethode/referenceaunconstructeur/Main.java
+++ src/main/java/corelab/structurededonnees/programmationfonctionnelle/lesreferencesdemethode/referenceaunconstructeur/Main.java
@@ -7,13 +7,35 @@ import java.util.function.Supplier;
 public class Main {
 
 	/****
-	 * exemple d'utilisation de référence a un constructeur permettant de créer de nouvelle 
-	 * instance de List .
+	 * exemple d'utilisation de référence a un constructeur permettant de créer de nouvelle  instance de la classe User
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Supplier<List<String>> supplier = ArrayList::new;
-		List<String> uneListeDeString = supplier.get();
+
+		Stream<String> users = Stream.of("user1","user2","user3","user4","user5","user6");
+
+		//sans méthode de référence 
+		users.map(e -> new User(e)).forEach(System.out::println);
+
+		//avec une méthode de référence pour évoquer le constructeur de User  
+		users.map(User::new).forEach(System.out::println);
+	}
+
+	class User{
+		private String name;
+
+		public User (String name){
+			this.name = name;
+		}
+
+		public Stirng getName(){
+			return this.name;
+		}
+
+		public void setName(String name){
+			this.name = name;
+		}
+		
 	}
 
 }
